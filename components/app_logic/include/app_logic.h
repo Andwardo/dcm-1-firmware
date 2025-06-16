69a81ba1a1539923533b2670a86a3bf9822a5d43
Andwardo <andward63@aim.com>
2025-06-16 08:43:35 -0500

    doesnt work but i ma passing off to new GPT at this point.

--- components/app_logic/include/app_logic.h
+++ components/app_logic/include/app_logic.h
@@ -1,21 +1,18 @@
 /* File: components/app_logic/include/app_logic.h
  *
- * Created on: 2025-06-11
- * Edited on:  2025-06-15 14:50:00 CDT
- * Version: v7.8.1
- * Author: R. Andrew Ballard (c) 2025
+ -* Version: v7.8.1
+ +* Version: v8.0.6
  */
 
 #pragma once
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+void app_logic_init(void);
+
 /**
  * @brief Start the application logic FreeRTOS task.
- *
- * This creates and starts the `app_task` that runs the main loop.
  */
 void app_logic_start_task(void);
 
