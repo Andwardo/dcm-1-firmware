5fa3de3ad39903fd86a85b55bc956689afdc11e6
Andwardo <andward63@aim.com>
2025-07-09 07:35:28 -0500

    Fix build error by adding wifi_manager_connect_sta; set SSID prefix and default password

--- components/wifi_manager/wifi_manager.h
+++ components/wifi_manager/wifi_manager.h
@@ -1,23 +1,31 @@
 /*
  *  wifi_manager.h
  *
- *  Created on: 2025-06-18
- *  Edited on: 2025-07-08
+ *  Created on: 2025-06-23
+ *  Edited on: 2025-07-09
  *      Author: Andwardo
- *      Version: v8.2.46
+ *      Version: v8.2.47
  */
 
-#ifndef WIFI_MANAGER_H_
-#define WIFI_MANAGER_H_
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
 
-#include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define WIFI_MANAGER_STA_CONNECTED_BIT BIT0
 
 void wifi_manager_start(void);
+void wifi_manager_connect_sta(const char* ssid, const char* password);
 EventGroupHandle_t wifi_manager_get_event_group(void);
 void generate_ap_ssid_from_mac(void);
 const char* get_ap_ssid(void);
 
-#endif /* WIFI_MANAGER_H_ */
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WIFI_MANAGER_H
